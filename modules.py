1d66dfd70cd1e96959bb1f2b83e5bdfb7aab6190
localghost1234 <viczaz99@hotmail.com>
2024-10-30 22:14:01 -0600

    refactored modules.py

--- modules.py
+++ modules.py
@@ -1,10 +1,9 @@
 import hashlib
 
-def hash_password(sample_password):
-    return hashlib.sha1(sample_password.encode('utf-8')).hexdigest()
-
 def add_salt(password, salt):
     return salt + password + salt
 
-def check_final_hash(original_hash, password):
-    return original_hash == hash_password(password)
+def check_final_hash(original_hash, sample_password):
+    new_hash = hashlib.sha1(sample_password.encode('utf-8')).hexdigest()
+
+    return original_hash == new_hash
