80b6806aca98ed19eed1e2acc7e049e577419688
localghost1234 <viczaz99@hotmail.com>
2024-10-30 21:00:22 -0600

    changed salt appending back to a single-lined function and changed its name

--- modules.py
+++ modules.py
@@ -3,13 +3,8 @@ import hashlib
 def hash_password(sample_password):
     return hashlib.sha1(sample_password.encode('utf-8')).hexdigest()
 
-def append_salt(password, salt):
-    salted_password_recipient = []
-    salted_password_recipient.append(salt)
-    salted_password_recipient.append(password)
-    salted_password_recipient.append(salt)
-
-    return ''.join(salted_password_recipient)
+def add_salt(password, salt):
+    return salt + password + salt
 
 def check_final_hash(original_hash, password):
     return original_hash == hash_password(password)
--- password_cracker.py
+++ password_cracker.py
@@ -1,13 +1,13 @@
 from constants import passwords_file_path, salts_file_path, default_message_text
-from modules import append_salt, check_final_hash
+from modules import add_salt, check_final_hash, hash_password
 
 def crack_sha1_hash(hash, use_salts = False):
     password_result = ''
     passwords_file = open(passwords_file_path, 'r')
 
     for sample_password in passwords_file:
-        clean_password = sample_password.strip()
         is_correct_password = False
+        clean_password = sample_password.strip()
 
         if not use_salts:
             is_correct_password = check_final_hash(hash, clean_password)
@@ -15,7 +15,7 @@ def crack_sha1_hash(hash, use_salts = False):
             salts_file = open(salts_file_path, 'r')
             
             for sample_salt in salts_file:
-                salted_password = append_salt(clean_password, sample_salt.strip())
+                salted_password = add_salt(clean_password, sample_salt.strip())
                 is_correct_password = check_final_hash(hash, salted_password)
 
                 if is_correct_password:
